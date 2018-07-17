67e76d9aad68a899ecbc398e2dcea533bffdffd0
이승우 주임 <sw.lee@hdel.co.kr>
2018-07-17 18:12:47 +0900

    2018-07-17 Summary 화면 작업 중

--- app/src/main/java/com/hb/sseung/godbody/MainActivity.java
+++ app/src/main/java/com/hb/sseung/godbody/MainActivity.java
@@ -1,7 +1,9 @@
 package com.hb.sseung.godbody;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+//        Button b = (Button)findViewById(R.id.button);
+//        b.setOnClickListener(new View.OnClickListener(){
+//            @Override
+//            public void onClick(View view){
+//                Intent intent = new Intent(getApplicationContext(), SummaryActivity.class);
+//                startActivity(intent);
+//            }
+//        });
     }
+
+    public void onButton(View view){
+        Intent intent = new Intent(getApplicationContext(), SummaryActivity.class);
+        startActivity(intent);
+    }
+
+
 }
